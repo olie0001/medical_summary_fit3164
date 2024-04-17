69df56ab6106c8565776e474e566eea254c1a3b6
Ovylia L <olie0001@student.monash.edu>
2024-04-17 20:38:41 +1000

    loop through one patient's note, with help of pei_test code. also added chief complaint extraction function

--- patient_history/app.py
+++ patient_history/app.py
@@ -1,44 +1,59 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from transformers import AutoTokenizer, BertLMHeadModel
+import re
 
-# NLP code
 def summarise(note):
-    # BERT's microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract model
     model_name = "microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract"
     model = BertLMHeadModel.from_pretrained(model_name, is_decoder=True)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # Tokenize the medical note using the this model and summarise the note
     inputs = tokenizer.encode("summarize: " + note, return_tensors="pt", max_length=512, truncation=True)
     summary_ids = model.generate(inputs, max_length=513, min_length=50, num_beams=4, early_stopping=True)
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
 
-app = Flask(__name__)
+def chief_complaint(note):
+    complaint = re.search(r'chief complaint\s*:\s*(.*?)(major surgical|history of present illness|$)', note, re.IGNORECASE)
+    if complaint:
+        return complaint.group(1).strip()
+    else:
+        return None
 
-# Configure the SQLAlchemy part to connect to the PostgreSQL database
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ds12@localhost/patient_note'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialize the database connection
 db = SQLAlchemy(app)
 
-# Define your database model
-class DischargeFirst10(db.Model):
-    __tablename__ = 'discharge_first_10'  # Your table name
-    note_id = db.Column(db.String(), primary_key=True)  # Assuming note_id is the primary key
-    text = db.Column(db.Text)  # Assuming 'text' is the name of the column to retrieve
+class DischargeData(db.Model):
+    __tablename__ = 'discharge_data'
+    note_id = db.Column(db.String(), primary_key=True)
+    storetime = db.Column(db.Date)
+    subject_id = db.Column(db.String(8), nullable=False)
+    text = db.Column(db.Text)
 
 @app.route('/')
 def index():
-    # Query the first 'text' entry from the discharge_first_10 table
-    note_entry = DischargeFirst10.query.first()
-    medical_note = note_entry.text
-    # Call the summarisation function 
-    note = summarise(medical_note)
-    return render_template('index.html', medical_note=note)
+    # Patient
+    subject_id = '10000032'
+
+    # Patient History
+    discharge_entries = DischargeData.query.filter_by(subject_id=subject_id).all()
+    discharge_arr = []
+    for entry in discharge_entries:
+        unsummarised_note = entry.text
+        summarised_note = summarise(unsummarised_note)
+        complaint = chief_complaint(summarised_note)
+        entry_data = {
+            'note_id': entry.note_id,
+            'storetime': entry.storetime,
+            'complaint': complaint,
+            'text': summarised_note,
+        }
+        discharge_arr.append(entry_data)
+
+    return render_template('index.html', discharge_arr=discharge_arr)
 
 if __name__ == '__main__':
     app.run(debug=True)
